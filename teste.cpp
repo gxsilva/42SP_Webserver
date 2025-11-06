00490132331d989509773eaafcb47e47eeebbe6e
gxsilva_user <lssvgabriel@gmail.com>
2025-11-05 21:59:25 -0300

    build(set-up): Configuring clang-format

---
+++ teste.cpp
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   teste.cpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cadete <cadete@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/11/05 20:53:03 by cadete            #+#    #+#             */
+/*   Updated: 2025/11/05 21:30:23 by cadete           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+
+class Peixoto
+{
+
+	public:
+	void hello(void) { std::cout << "hello" << std::endl; }
+}
+
+int main(void)
+{
+	std::cout << " hello world" << std::endl;
+	return (0);
+}
