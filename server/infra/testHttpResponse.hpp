058765551dc696b105776f0b0206aac3587b5295
Fabio <fabiolljunior23@gmail.com>
2026-03-07 20:15:04 -0300

    refactor(server): removed and added to srcs dir

--- server/infra/testHttpResponse.hpp
+++
@@ -1,9 +0,0 @@
-#ifndef TESTHTTPRESPONSE_HPP
-#define TESTHTTPRESPONSE_HPP
-
-#include "clientSocket.hpp"
-#include <cstring>
-
-void bufferTestHttpResponse(ClientSocket& client);
-
-#endif
--- server/services/serverHandlers.cpp
+++
@@ -1,7 +0,0 @@
-#include "server.hpp"
-
-bool    Server::handleError(const std::string& msg)
-{
-    perror(msg.c_str());
-    return (false);
-}
