2169811aa7e3fb18e4d0281db94a7400a055e452
Fabio <fabiolljunior23@gmail.com>
2026-03-12 19:33:04 -0300

    feat(CgiHandler): implement CgiHandler class for managing CGI execution and responses

---
+++ src/application/CGI/CgiHandler.hpp
@@ -0,0 +1,42 @@
+#ifndef CGIHANDLER_HPP
+#define CGIHANDLER_HPP
+
+#include "../../domain/entities/HttpRequest.hpp"
+#include "../../interfaces/port/InitCgiGateway.hpp"
+#include "../../domain/value_objects/CgiEnvironment.hpp"
+#include "../../domain/value_objects/CgiResponse.hpp"
+#include <string>
+
+struct CgiRouteConfig
+    {
+        std::string scriptPath;
+        std::string interpreterPath;
+        std::string serverName;
+        int         serverPort;
+    };
+
+    class CgiHandler
+    {
+    public:
+        CgiHandler(InitCgiGateway& gateway, const CgiRouteConfig& config);
+        ~CgiHandler();
+
+        // Inicia execução CGI, retorna false se falhar
+        bool startCgi(const HttpRequest& request);
+
+        // Chamado pelo event loop quando poll() sinaliza readiness
+        InitCgiGateway& getGateway();
+
+        // Monta HttpResponse quando CGI termina
+        HttpResponse buildResponse();
+
+    private:
+        CgiHandler();
+        CgiHandler(const CgiHandler&);
+        CgiHandler& operator=(const CgiHandler&);
+
+        InitCgiGateway&   _gateway;
+        CgiRouteConfig _config;
+};
+
+#endif
