18d1017c24d804abca07060d84deeab1de02619c
Fabio <fabiolljunior23@gmail.com>
2026-03-17 22:01:39 -0300

    feat(PostRequestHandler): add POST request handler implementation

---
+++ src/application/methods/PostRequestHandler.hpp
@@ -0,0 +1,39 @@
+#ifndef POSTREQUESTHANDLER_HPP
+#define POSTREQUESTHANDLER_HPP
+
+#include "../../domain/entities/HttpRequest.hpp"
+#include "../../domain/entities/HttpResponse.hpp"
+#include "../../domain/entities/server/ServerBlock.hpp"
+#include "../../domain/value_objects/HttpStatusCode.hpp"
+
+#include <string>
+
+class PostRequestHandler
+{
+	public:
+		PostRequestHandler();
+		~PostRequestHandler();
+
+		void		 configure(const ServerBlock& serverConfig);
+		HttpResponse handle(const HttpRequest& request);
+
+	private:
+		ServerBlock _serverConfig;
+		bool		_hasServerConfig;
+
+		std::string			 stripUriQuery(const std::string& uri) const;
+		const LocationBlock* findBestLocation(const std::string& uriPath) const;
+		std::string			 resolveRoot(const LocationBlock* location) const;
+		std::string			 resolveFilePath(const std::string& root, const std::string& uriPath) const;
+		std::string			 parentDirectory(const std::string& path) const;
+		std::string			 buildUploadTarget(const std::string& filePath) const;
+		bool				 isMethodAllowed(const LocationBlock* location) const;
+		HttpResponse		 writeRequestBody(const HttpRequest& request, const std::string& targetPath) const;
+		HttpResponse		 buildCreatedResponse(const std::string& targetPath) const;
+		HttpResponse		 buildErrorResponse(HttpStatusCode code) const;
+
+		PostRequestHandler(const PostRequestHandler&);
+		PostRequestHandler& operator=(const PostRequestHandler&);
+};
+
+#endif
